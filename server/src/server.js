d24d77e6d2bcba401cbf061c6b0b837731ff4e4e
aditya gore <aditya_gore@hotmail.com>
2023-08-21 08:07:35 +0530

    minor

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,15 @@
-const http = require("http");
+const http = require('http');
 
-const app = require("./app");
+const app = require('./app');
 
-const { loadPlanetsData } = require("./models/planets.model");
+const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
 async function startServer() {
-  await loadPlanetsData();
+  await loadPlanetsData(); // executes as soon as server starts
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
