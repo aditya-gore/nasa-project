473569f1223e8ee02740608d3f138d52ac2d4726
aditya gore <aditya_gore@hotmail.com>
2023-08-22 08:29:20 +0530

    get launches done

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,47 +1,38 @@
-const fs = require('fs');
-const path = require('path');
-const { parse } = require('csv-parse');
+const fs = require("fs");
+const path = require("path");
+const { parse } = require("csv-parse");
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
   return (
-    planet['koi_disposition'] === 'CONFIRMED' &&
-    planet['koi_insol'] > 0.36 &&
-    planet['koi_insol'] < 1.11 &&
-    planet['koi_prad'] < 1.6
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
   );
 }
-/**
- * const promise = new Promise((resolve, reject) => {
- *  resolve(42);
- * })
- * promise.then((result) => {
- *
- * });
- * const result = await promise;
- * console.log(Result);
- */
+
 function loadPlanetsData() {
   return new Promise((resolve, reject) => {
     fs.createReadStream(
-      path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'),
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv"),
     )
       .pipe(
         parse({
-          comment: '#',
+          comment: "#",
           columns: true,
         }),
       )
-      .on('data', (data) => {
+      .on("data", (data) => {
         if (isHabitablePlanet(data)) {
           habitablePlanets.push(data);
         }
       })
-      .on('error', (err) => {
+      .on("error", (err) => {
         console.log(err);
         reject(err);
       })
-      .on('end', () => {
+      .on("end", () => {
         resolve();
       });
   });
